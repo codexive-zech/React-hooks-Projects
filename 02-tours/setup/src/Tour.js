939fc3579f9f395f211862c203663ad314aad859
Codexive_zech <zechariahhounwanou@gmail.com>
2022-06-23 21:01:57 +0100

    added few tour comments for clarity

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 const Tour = ({ id, name, image, info, price, removeTour }) => {
-  const [readMore, setReadMore] = useState(false);
+  const [readMore, setReadMore] = useState(false); //define the use state for read more
   const handleReadMoreClick = () => {
-    setReadMore(!readMore);
-  };
+    setReadMore(!readMore); //set read more state value as the opposite of initial state
+  }; // a func handling the read more button
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -14,8 +14,10 @@ const Tour = ({ id, name, image, info, price, removeTour }) => {
           <h4 className="tour-price">{price}</h4>
         </div>
         <p>
+          {/* if read more state is true then fetch the info data else let the returned info value be between 0 and 290*/}
           {readMore ? info : `${info.substring(0, 290)}...`}
           <button onClick={handleReadMoreClick}>
+            {/* if read more state is true then display show less else display show more */}
             {readMore ? "show less" : "show more..."}
           </button>
         </p>
