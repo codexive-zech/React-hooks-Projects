8e934d8b6e0caa04b89e0beea2af61662349442d
Codexive_zech <zechariahhounwanou@gmail.com>
2022-06-24 17:36:14 +0100

    Implemented the accordion functionality on eacg question

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false); //define state for show info
+  const handleShowInfo = () => {
+    setShowInfo(!showInfo);
+  }; //update the show info state on a click to the opposite
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={handleShowInfo}>
+          {/* if show info state is true then display the minus else display plus */}
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {/* show info data only if info is true */}
+      {showInfo && <p> {info}</p>}
+    </article>
+  );
 };
 
 export default Question;
