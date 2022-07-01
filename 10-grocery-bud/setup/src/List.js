a4ce7902cf34a8262ce116e5ff5b69dda90b10b8
Codexive_zech <zechariahhounwanou@gmail.com>
2022-07-02 00:01:40 +0100

    Implemented a functional CRUD grocery-bud with local storage for data
    Added Create, Edit, Delete functionaliy for grocery-bud, alert and local storage

--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = ({ items }) => {
+// destructing List component props added
+const List = ({ items, removeGroceryItem, editGroceryItem }) => {
   return (
     <div className="grocery-list">
+      {/* iterating over the entire grocery list item to return single grocery item */}
       {items.map((item) => {
         const { id, title } = item;
         return (
           <article key={id} className="grocery-item">
             <p className="title">{title}</p>
             <div className="btn-container">
-              <button className="edit-btn">
+              <button
+                className="edit-btn"
+                onClick={() => editGroceryItem(id)} // a click that handles editing of grocery items
+              >
                 <FaEdit />
               </button>
-              <button className="delete-btn">
+              <button
+                className="delete-btn"
+                onClick={() => removeGroceryItem(id)} // a click that handles deleting of grocery items
+              >
                 <FaTrash />
               </button>
             </div>
