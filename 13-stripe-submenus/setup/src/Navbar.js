2b156a4a1870b2b0f9ce6f73c3054240b0c766db
Codexive_zech <zechariahhounwanou@gmail.com>
2022-07-04 21:39:26 +0100

    Implented a full stripe kinda menu bar that has submenus when hovered over

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -6,13 +6,21 @@ import { useGlobalContext } from "./context";
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
 
-  // const displaySubmenu = (e) => {
-  //   console.log("hello");
-  //   openSubmenu();
-  //   onMouseOver event
-  // };
+  const displaySubmenu = (e) => {
+    const pageText = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 2;
+    openSubmenu(pageText, { center, bottom });
+  };
+
+  const handleCloseSubmenu = (e) => {
+    if (!e.target.classList.contains("link-btn")) {
+      closeSubmenu();
+    }
+  };
   return (
-    <nav className="nav">
+    <nav className="nav" onMouseOver={handleCloseSubmenu}>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="stripe" className="nav-logo" />
@@ -22,13 +30,19 @@ const Navbar = () => {
         </div>
         <ul className="nav-links">
           <li>
-            <button className="link-btn">products</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button className="link-btn">developers</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
           <li>
-            <button className="link-btn">company</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
         </ul>
         <button className="btn signin-btn">Sign in</button>
