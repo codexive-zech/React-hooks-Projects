ec1053735a3646b06654f8a2f23f148dd6c8a018
Codexive_zech <zechariahhounwanou@gmail.com>
2022-07-04 10:33:35 +0100

    Started Impementing a kinda stripe submenu to display menu categories when hovered

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,7 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  // const { isSubmenuOpen } = useGlobalContext;
+  return (
+    <h1></h1>
+    // <aside className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}>
+    //   submenu
+    // </aside>
+  );
+};
 
-export default Submenu
+export default Submenu;
