91c2de556060f76383adcd415e1dd469674f114f
Codexive_zech <zechariahhounwanou@gmail.com>
2022-07-12 22:18:08 +0100

    Added clearer comments for understanding and removed final files

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -4,20 +4,22 @@ import Loading from "./Loading";
 import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
-  const { loading, cocktailList } = useGlobalContext();
+  const { loading, cocktailList } = useGlobalContext(); // define a loading state
+
   if (loading) {
     return <Loading />;
-  }
+  } // display when loading state is true
 
   if (cocktailList.length < 1) {
     return <h2 className="section-title">No Cocktail Matches the Criteria</h2>;
-  }
+  } //display when the length of the cocktail is less than 1
   return (
     <section className="section">
       <h2 className="section-title">cocktails</h2>
+      {/* iterate over the cocktail list */}
       <section className="cocktails-center">
         {cocktailList.map((cocktail) => {
-          return <Cocktail key={cocktail.id} {...cocktail}/>
+          return <Cocktail key={cocktail.id} {...cocktail} />;
         })}
       </section>
     </section>
