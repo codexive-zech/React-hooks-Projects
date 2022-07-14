a84c4e33e38ec74d2df84f6255cdde7618a2de60
Codexive_zech <zechariahhounwanou@gmail.com>
2022-07-14 21:59:07 +0100

    Implemented Pagination functionality
    Understand the logic to creating array from an existing array for pagination, learnt how to setup data fetch based on page and more

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,11 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const followersPerPage = 12;
+  const pages = Math.ceil(followers.length / followersPerPage);
+  const followersByPage = Array.from({ length: pages }, (_, index) => {
+    const start = index * followersPerPage;
+    return followers.slice(start, start + followersPerPage);
+  });
+  return followersByPage;
+};
 
-export default paginate
+export default paginate;
