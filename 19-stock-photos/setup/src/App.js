8b5362e380dc969ea7405255adb2c49a686ac78c
Codexive_zech <zechariahhounwanou@gmail.com>
2022-07-16 15:07:49 +0100

    Implemented search query functionality and proper loading or images searched

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -9,33 +9,31 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photoList, setPhotoList] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("");
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    const pageUrl = `&page${page}`;
-    url = `${mainUrl}${clientIdKey}${pageUrl}`;
+    const pageUrl = `&page=${page}`;
+    const queryUrl = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${clientIdKey}${pageUrl}${queryUrl}`;
+    } else {
+      url = `${mainUrl}${clientIdKey}${pageUrl}`;
+    }
     try {
       const response = await fetch(url);
       const data = await response.json();
-      const newPhotoDetails = data.map((photoDetails) => {
-        const { alt_description, likes, id } = photoDetails;
-        const { regular } = photoDetails.urls;
-        const { name, portfolio_url } = photoDetails.user;
-        const { medium } = photoDetails.user.profile_image;
-        return {
-          id,
-          alt_description,
-          likes,
-          regular,
-          name,
-          portfolio_url,
-          medium,
-        };
-      });
-      setPhotoList((oldPhotoList) => {
-        return [...oldPhotoList, ...newPhotoDetails];
+      setPhotoList((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
       });
       setLoading(false);
     } catch (error) {
@@ -46,11 +44,12 @@ function App() {
 
   const handleForm = (e) => {
     e.preventDefault();
-    console.log("handling Form");
+    setPage(1);
   };
 
   useEffect(() => {
     fetchImages();
+    // eslint-disable-next-line
   }, [page]);
 
   useEffect(() => {
@@ -67,12 +66,20 @@ function App() {
       }
     });
     return () => window.removeEventListener("scroll", event);
+    // eslint-disable-next-line
   }, []);
+
   return (
     <main>
       <section className="search">
         <form className="search-form">
-          <input type="text" className="form-input" placeholder="Search" />
+          <input
+            type="text"
+            className="form-input"
+            placeholder="Search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <button type="submit" className="submit-btn" onClick={handleForm}>
             <FaSearch />
           </button>
