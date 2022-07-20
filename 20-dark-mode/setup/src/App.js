71c718d59b8346979705db61ba50d1760e5fb800
Codexive_zech <zechariahhounwanou@gmail.com>
2022-07-20 01:33:57 +0100

    Added clearer comments and removed unneccessary files

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -3,39 +3,46 @@ import data from "./data";
 import Article from "./Article";
 
 const getStorageTheme = () => {
-  let theme = "light-theme";
-  const getTheme = localStorage.getItem("theme");
+  let theme = "light-theme"; // default theme
+  const getTheme = localStorage.getItem("theme"); // get theme in the local storage
+  // checking which theme exist in the local storage
   if (getTheme) {
-    theme = getTheme;
+    theme = getTheme; // change theme to the theme found in the local storage
   }
   return theme;
-};
+}; // func get theme from local storage
 
 function App() {
   // eslint-disable-next-line
-  const [blogs, setBlogs] = useState(data);
-  const [theme, setTheme] = useState(getStorageTheme());
+  const [blogs, setBlogs] = useState(data); // define a blogs state
+  const [theme, setTheme] = useState(getStorageTheme()); // define a theme state
 
   useEffect(() => {
-    localStorage.setItem("theme", theme);
-    document.documentElement.className = theme;
-  }, [theme]);
+    localStorage.setItem("theme", theme); // set up local storage for theme color style
+    document.documentElement.className = theme; // set the overall html class to theme state
+  }, [theme]); // re-render only when theme state changes
+
   const toggleTheme = () => {
+    // checking theme state value
     if (theme === "light-theme") {
-      setTheme("dark-theme");
+      setTheme("dark-theme"); // set theme state
     } else {
-      setTheme("light-theme");
+      setTheme("light-theme"); //set theme state
     }
-  };
+  }; // func changes theme state when toggled
   return (
     <main>
       <nav className="nav-center">
         <h1>Overreacted</h1>
-        <button className="btn" onClick={toggleTheme}>
+        <button
+          className="btn"
+          onClick={toggleTheme} // a toggle theme click button
+        >
           Toggle
         </button>
       </nav>
       <section className="articles">
+        {/* iterate over the list of blogs */}
         {blogs.map((blog) => {
           return <Article key={blog.id} {...blog} />;
         })}
--- 20-dark-mode/setup/src/Article.js
+++ 20-dark-mode/setup/src/Article.js
@@ -1,10 +1,12 @@
 import React from "react";
 import moment from "moment";
+// destructed props in the article array
 const Article = ({ title, date, length, snippet }) => {
   return (
     <article className="post">
       <h2>{title}</h2>
       <div className="post-info">
+        {/* added moment.js library */}
         <span>{moment(date).format("Do dddd MMM YYYY")}</span>
         <span>{length} Min Read</span>
       </div>
