cbaec53428412673d34facdfd7c438250fa6a101
Codexive_zech <zechariahhounwanou@gmail.com>
2022-07-20 01:20:00 +0100

    Implemeted a Light and Dark Theme toggle

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -1,9 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
+
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  const getTheme = localStorage.getItem("theme");
+  if (getTheme) {
+    theme = getTheme;
+  }
+  return theme;
+};
 
 function App() {
-  return <h2>dark mode starter</h2>
+  // eslint-disable-next-line
+  const [blogs, setBlogs] = useState(data);
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.documentElement.className = theme;
+  }, [theme]);
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+  return (
+    <main>
+      <nav className="nav-center">
+        <h1>Overreacted</h1>
+        <button className="btn" onClick={toggleTheme}>
+          Toggle
+        </button>
+      </nav>
+      <section className="articles">
+        {blogs.map((blog) => {
+          return <Article key={blog.id} {...blog} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 20-dark-mode/setup/src/Article.js
+++ 20-dark-mode/setup/src/Article.js
@@ -1,7 +1,16 @@
-import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
-}
+import React from "react";
+import moment from "moment";
+const Article = ({ title, date, length, snippet }) => {
+  return (
+    <article className="post">
+      <h2>{title}</h2>
+      <div className="post-info">
+        <span>{moment(date).format("Do dddd MMM YYYY")}</span>
+        <span>{length} Min Read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  );
+};
 
-export default Article
+export default Article;
