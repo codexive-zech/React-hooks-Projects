60585d291be7cae276529291a5f68c905fe47a2a
Codexive_zech <zechariahhounwanou@gmail.com>
2022-07-22 02:56:47 +0100

    Implemented a Movie search engine app

--- 21-movie-db/setup/src/context.js
+++ 21-movie-db/setup/src/context.js
@@ -1,14 +1,47 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from "react";
 // make sure to use https
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
-const AppContext = React.createContext()
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [isLoading, setIsLoading] = useState(true);
+  const [movieList, setMovieList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("sup");
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      // console.log(data);
+      if (data.Response === "True") {
+        setMovieList(data.Search);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${searchQuery}`);
+  }, [searchQuery]);
+  return (
+    <AppContext.Provider
+      value={{ isLoading, movieList, error, searchQuery, setSearchQuery }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
